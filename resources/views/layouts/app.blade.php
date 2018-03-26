6a9988cc19be180d7dbc9df6ed816823b6bd6ede
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-03-27 01:17:44 +0200

    Added link to user profile in user menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
                                 @if(\Auth::user()->houses()->count() >= 1)
                                 <a class="dropdown-item" href="{{ route('admin.dashboard') }}">Gestisci le tue case</a>
                                 @endif
-                                <a class="dropdown-item" href="{{ route('user.edit') }}">Modifica il tuo profilo</a>
+                                <a class="dropdown-item" href="{{ route('user.profile', \Auth::user()->id) }}">Visualizza il tuo profilo</a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Esci da Renteet</a>
                                 
