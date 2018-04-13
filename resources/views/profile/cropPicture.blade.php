1788791e7a51cdeaaec0614bb6febdcfa96ba0d0
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-04-13 19:41:23 +0200

    Layout fix - Created migration

--- resources/views/profile/cropPicture.blade.php
+++ resources/views/profile/cropPicture.blade.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.2/croppie.min.css" />
 <style>
 .croppie-container .cr-boundary {
-    width: 500px;
+    width: 100%;
     height: 500px;
 }
 </style>
@@ -36,24 +36,56 @@
 @endsection
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid margin-top-20">
+        <h1 class="page-title">{{'Ritaglia la tua immagine del profilo'}}</h1>
         <div class="row margin-top-40">
-            <div class="col-md-12 col-md-offset-2">
+            <div class="col-md-6">
                 <div class="panel panel-default">
-                    <h1 class="page-title">{{'Ritaglia la tua immagine del profilo'}}</div>
-
                     <form id="crop-form" class="form-horizontal" method="POST" action="{{ route('crop-picture') }}" enctype="multipart/form-data">
                         {{ csrf_field() }}
 
-                        <div class="panel-body text-center">
+                        <div class="panel-body text-right">
                             <div id="image-to-crop"></div>
 
-                            <button type="button" id="crop-btn" class="btn btn-success">Salva</button>
+                            <button type="button" id="crop-btn" class="btn btn-lg btn-success"><i class="fa fa-check"></i> Ritaglia</button>
                         </div>
 
-                        <input type="hidden" id="blob" name="blob">
+                        <input type="hidden" id="blob" name="blob" />
                     </form>
                 </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="panel-body" style="padding: 60px">
+
+                    <h3>Scegli un'immagine che ti rappresenti così i tuoi coinquilini potranno vedere quanto sei troia e richiedere di entrare nelle case in cui stai per poterti trombare fino a farti piangere.</h3>
+
+                    <form class="form-horizontal margin-top-40" method="POST" action="{{ route('upload-picture') }}" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('profile_picture') ? ' has-error' : '' }}">
+                            <label for="profile_picture" class="col-md-4 control-label">Scegli un'immagine che ti rappresenta</label>
+
+                            <div class="col-md-6">
+                                <input id="profile_picture" type="file" class="form-control" name="profile_picture" value="{{ old('profile_picture') }}" required autofocus>
+
+                                @if ($errors->has('profile_picture'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('profile_picture') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4 margin-top-40">
+                                <button type="submit" class="btn btn-primary">
+                                    Cambia la tua immagine del profilo
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>            
             </div>
         </div>
     </div>
--- resources/views/profile/uploadPicture.blade.php
+++ resources/views/profile/uploadPicture.blade.php
@@ -3,39 +3,41 @@
 @section('title', 'Completa il tuo profilo')
 
 @section('content')
-<div class="container">
+<div class="container-fluid margin-top-20">
+    <h1 class="page-title">{{'Ritaglia la tua immagine del profilo'}}</h1>
     <div class="row margin-top-40">
-        <div class="col-md-12 col-md-offset-2">
-            <div class="panel panel-default">
-                <h1 class="page-title">Carica un'immagine del profilo</div>
+        <div class="col-md-6">
+            <div class="panel-body">
+                <h3>Scegli un'immagine che ti rappresenti così i tuoi coinquilini potranno vedere quanto sei troia e richiedere di entrare nelle case in cui stai per poterti trombare fino a farti piangere.</h3>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="panel-body">
+                <form class="form-horizontal" method="POST" action="{{ route('upload-picture') }}" enctype="multipart/form-data">
+                    {{ csrf_field() }}
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('upload-picture') }}" enctype="multipart/form-data">
-                        {{ csrf_field() }}
+                    <div class="form-group{{ $errors->has('profile_picture') ? ' has-error' : '' }}">
+                        <label for="profile_picture" class="col-md-4 control-label">Scegli un'immagine che ti rappresenta</label>
 
-                        <div class="form-group{{ $errors->has('profile_picture') ? ' has-error' : '' }}">
-                            <label for="profile_picture" class="col-md-4 control-label">Scegli un'immagine che ti rappresenta</label>
+                        <div class="col-md-6">
+                            <input id="profile_picture" type="file" class="form-control" name="profile_picture" value="{{ old('profile_picture') }}" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="profile_picture" type="file" class="form-control" name="profile_picture" value="{{ old('profile_picture') }}" required autofocus>
-
-                                @if ($errors->has('profile_picture'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('profile_picture') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            @if ($errors->has('profile_picture'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('profile_picture') }}</strong>
+                                </span>
+                            @endif
                         </div>
+                    </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Carica la tua immagine del profilo
-                                </button>
-                            </div>
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <button type="submit" class="btn btn-primary">
+                                Carica la tua immagine del profilo
+                            </button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
