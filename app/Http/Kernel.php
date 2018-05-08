3b335f870d785ee2f9a28cda95d4e19fa82cf1c8
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-05-08 22:41:00 +0200

    Added middleware to check if profile is completed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'profile.complete' => \App\Http\Middleware\ProfileComplete::class,
     ];
 }
