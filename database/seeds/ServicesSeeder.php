d8949cf511ce75941a259ffd5ad3daca2a09fcac
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-05-19 15:03:37 +0200

    Removed 'App\' before Service model

--- database/seeds/ServicesSeeder.php
+++ database/seeds/ServicesSeeder.php
@@ -13,17 +13,17 @@ class ServicesSeeder extends Seeder
     public function run()
     {
         // TODO: trovare le icone giuste
-        App\Service::create(['name' => 'TV', 'icon_class' => 'fa-tv']);
-        App\Service::create(['name' => 'Termosifone', 'icon_class' => 'fa-th']);
-        App\Service::create(['name' => 'Camino', 'icon_class' => 'fa-fire']);
-        App\Service::create(['name' => 'Aria condizionata', 'icon_class' => 'fa-thermometer-half']);
-        App\Service::create(['name' => 'Scrivania', 'icon_class' => 'fa-fire']);
-        App\Service::create(['name' => 'Doccia', 'icon_class' => 'fa-fire']);
-        App\Service::create(['name' => 'Vasca da bagno', 'icon_class' => 'fa-fire']);
-        App\Service::create(['name' => 'Internet', 'icon_class' => 'fa-fire']);
-        App\Service::create(['name' => 'Forno', 'icon_class' => 'fa-fire']);
-        App\Service::create(['name' => 'Garage', 'icon_class' => 'fa-fire']);
-        App\Service::create(['name' => 'Phon', 'icon_class' => 'fa-fire']);
-        App\Service::create(['name' => 'Ferro da stiro', 'icon_class' => 'fa-fire']);
+        Service::create(['name' => 'TV', 'icon_class' => 'fa-tv']);
+        Service::create(['name' => 'Termosifone', 'icon_class' => 'fa-th']);
+        Service::create(['name' => 'Camino', 'icon_class' => 'fa-fire']);
+        Service::create(['name' => 'Aria condizionata', 'icon_class' => 'fa-thermometer-half']);
+        Service::create(['name' => 'Scrivania', 'icon_class' => 'fa-fire']);
+        Service::create(['name' => 'Doccia', 'icon_class' => 'fa-fire']);
+        Service::create(['name' => 'Vasca da bagno', 'icon_class' => 'fa-fire']);
+        Service::create(['name' => 'Internet', 'icon_class' => 'fa-fire']);
+        Service::create(['name' => 'Forno', 'icon_class' => 'fa-fire']);
+        Service::create(['name' => 'Garage', 'icon_class' => 'fa-fire']);
+        Service::create(['name' => 'Phon', 'icon_class' => 'fa-fire']);
+        Service::create(['name' => 'Ferro da stiro', 'icon_class' => 'fa-fire']);
     }
 }
