12b18a247b7db4b082730786c4cf4ec69440850d
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-05-19 17:20:11 +0200

    Added completed column to houses

---
+++ database/migrations/2018_05_19_151850_add_completed_column_to_houses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCompletedColumnToHousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('houses', function (Blueprint $table) {
+            $table->boolean('completed')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('houses', function (Blueprint $table) {
+            $table->dropColumn('completed');
+        });
+    }
+}
