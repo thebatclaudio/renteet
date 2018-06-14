9109af63621c527ed3fddd45cf69918d5d7eb44a
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-06-15 00:48:37 +0200

    Homepage restyle init (issue #6)

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+        <title>{{ config('app.name', 'Renteet') }} - @yield('title')</title>
+    
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">        <link rel="stylesheet"href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.5/jquery.fullpage.min.css" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+        <link rel="stylesheet" href="/css/app.css?{{rand()}}">
+        <link rel="stylesheet" href="/css/utilities.css?{{rand()}}">
+
+        @yield('styles')
+    </head>
+    <body>
+
+        <header>
+            <nav id="home-navbar" class="transparent-navbar navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                <a class="navbar-brand" href="{{url('/')}}" title="{{ config('app.name', 'Renteet') }} - Home">
+                    <img class="logo" src="/images/renteet-logo.png">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                </div>
+
+                <ul class="navbar-nav mr-auto hidden-xs flex-row-reverse">
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Accedi</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Iscriviti</a></li>
+                </ul>
+            </nav>
+        </header>
+        
+        <main role="main">
+            @yield('content')
+
+            <!-- FOOTER -->
+            <!--footer class="container">
+                <p class="float-right"><a href="#">Torna su</a></p>
+                <p>&copy; {{date('Y')}} Renteet, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+            </footer-->
+
+        </main>
+
+        <script>
+
+        var autocomplete = null;
+        function initMap() {
+            var input = document.getElementById('search-input');
+    
+            autocomplete = new google.maps.places.Autocomplete(input);
+    
+            autocomplete.addListener('place_changed', function() {
+                var place = autocomplete.getPlace();
+                if (!place.geometry) {
+                    // User entered the name of a Place that was not suggested and
+                    // pressed the Enter key, or the Place Details request failed.
+                    return;
+                }
+        
+                var address = '';
+                if (place.address_components) {
+                    address = [
+                    (place.address_components[0] && place.address_components[0].short_name || ''),
+                    (place.address_components[1] && place.address_components[1].short_name || ''),
+                    (place.address_components[2] && place.address_components[2].short_name || '')
+                    ].join(' ');
+                }
+
+                $("#lat").val(place.geometry.location.lat());
+                $("#lng").val(place.geometry.location.lng());
+
+                $("#searchForm").submit();
+
+            });
+        }
+
+        function geolocate() {
+            if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var geolocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                var circle = new google.maps.Circle({
+                    center: geolocation,
+                    radius: position.coords.accuracy
+                });
+                autocomplete.setBounds(circle.getBounds());
+            });
+            }
+        }
+        </script>
+        <script src="//maps.googleapis.com/maps/api/js?key=AIzaSyAgn7e4Tc95WlmbyqCz71oGDctx3rXf6oQ&libraries=places&callback=initMap"
+            async defer></script>
+
+        <script src="//code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+        <script src="//js.pusher.com/3.1/pusher.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.5/jquery.fullpage.min.js"></script>
+        <script src="//unpkg.com/sweetalert2@7.20.8/dist/sweetalert2.all.js"></script>
+        <script src="{{url('/js/notify.min.js')}}"></script>
+
+        <!-- Scripts -->
+        @yield('scripts')
+
+        <script>
+        $(document).ready(function() {
+            $(window).keydown(function(event){
+                if(event.keyCode == 13) {
+                    if($(":focus").attr('id') == 'search-input'){
+                        event.preventDefault();
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            });
+        });
+        </script>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,137 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.home')
 
 @section('title', 'Be friendly')
 
 @section('styles')
-<style>
-/* Style for our header texts
-* --------------------------------------- */
-h1, h2, h3{
-  margin:0;
-  padding:0;
-}
-/* Centered texts in each section
-* --------------------------------------- */
-.section{
-  text-align:center;
-}
-/* Backgrounds will cover all the section
-* --------------------------------------- */
-.section{
-  background-size: cover;
-  background-position: center center;
-}
-.slide{
-  background-size: cover;
-  background-position: center center;
-}
-/* Defining each section background and styles
-* --------------------------------------- */
-#section0{
-  background-image: url(images/bg1.jpg);
-}
-.blur {
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.2);
-}
-.padded-content {
-  padding: 120px;
-}
-.content {
-  top: 50%;
-  transform: translateY(-50%);
-  position: relative;
-}
-.content h1 {
-  color: #FFF;
-  font-size: 12em;
-}
-.full-height {
-  height: 100%;
-}
-.padded-content {
-  background-color: #FFF;
-  color: #000;
-}
-.padded-content h3 {
-  font-size: 4em;
-  margin-bottom: 40px;
-}
-.padded-content h3 {
-  font-size: 4em;
-  margin-bottom: 40px;
-}
-.padded-content p {
-  font-size: 2.6em;
-}
-#section2{
-  background-image: url(images/bg2.jpg);
-  padding: 0;
-}
-#section2 .padded-content {
-  background-color: rgba(254, 211, 127, 0.70);
-}
-#section3{
-  background-image: url(images/bg3.jpg);
-  padding: 0;
-}
-#section3 .padded-content {
-  background-color: rgba(253, 127, 124, 0.70);
-  color: #000;
-}
-#section4{
-  background-image: url(images/bg2.jpg);
-  padding: 0;
-}
-#section4 .padded-content {
-  background-color: rgba(19, 142, 83, 0.70);
-  color: #fff;
-}
-/*Adding background for the slides
-* --------------------------------------- */
-#slide1{
-  background-image: url(images/bg4.jpg);
-  padding: 0;
-}
-#slide2{
-  background-image: url(images/bg5.jpg);
-  padding: 0;
-}
-/* Bottom menu
-* --------------------------------------- */
-#infoMenu li a {
-  color: #fff;
-}
-
-.btn-big {
-  font-size: 2em;
-  padding: 10px 40px;
-  margin-top: 20px;
-  background: none;
-  color: #000;
-  border-color: #000;
-  border-width: 3px;
-}
-  
-
-#section2 .btn-big:hover, #section2 .btn-big:focus, #section2 .btn-big:active {
-  background-color: #e69800;
-  border-color: #dc9100;
-  box-shadow: 0 0 0 0;
-}
-
-#section3 .btn-big:hover, #section3 .btn-big:focus, #section3 .btn-big:active {
-  background-color: #ab3c5f;
-  border-color: #9e3f5d;
-  box-shadow: 0 0 0 0;
-}
-
-#section4 .btn-big:hover, #section4 .btn-big:focus, #section4 .btn-big:active {
-  background-color: #085833;
-  border-color: #094e2e;
-  box-shadow: 0 0 0 0;
-}
-</style>
+<link rel="stylesheet" href="/css/home.css?{{rand()}}">
 @endsection
 
 @section('content')
@@ -139,52 +11,106 @@ h1, h2, h3{
   <div class="section" id="section0">
     <div class="blur">
       <div class="content">
-        <img id="homepage-logo" src="/images/renteet-logo.png">
-        <h1>renteet</h1>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-5">
+              <h1>Renteet</h1>
+              <h2>Find interesting people to live with</h2>
+              <form id="searchForm" class="form-inline margin-top-20" action="{{route('search.coordinates')}}" method="GET">
+                  <input id="lat" name="lat" type="hidden" required>
+                  <input id="lng" name="lng" type="hidden" required>
+                  <input id="search-input" name="searchInput" class="form-control" type="text" onFocus="geolocate()" placeholder="Prova &quot;Palermo&quot;" aria-label="Cerca">
+                  <i class="search-icon fa fa-search fa-2x" aria-hidden="true"></i>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
   <div class="section" id="section2">
-    <div class="blur">
-      <div class="row full-height">
-        <div class="col-md-6 offset-md-6 text-left padded-content">
-          <h3>Trova la tua casa, oppure condividila!</h3>
-          <p>Condividi la tua casa con persone con i tuoi stessi interessi e cambia il tuo modo di vivere la vita da fuori sede!</p>
-          <a href="/register" title="Registrati" class="btn btn-big btn-primary">Scopri di più</a>
+      <div class="content">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-7">
+              <video width="500px" height="auto" autoplay="autoplay" loop="loop">
+                <source src="{{url('videos/blending-animation.mp4')}}" type="video/mp4" />
+              </video>
+            </div>
+            <div class="col-md-4 text-center">
+              <h3 class="claim">Oltre a <strong>dove</strong> vivere scegli <strong>con chi</strong> vivere</h3>
+              <h4 class="sub-claim">Trova persone con cui condividere casa in un modo in cui non l'hai mai fatto</h4>
+              <a href="{{url('/come-funziona/')}}" title="Come funziona" class="btn btn-danger btn-how-it-works margin-top-20">Come funziona</a>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
   </div>
   <div class="section" id="section3">
-    <div class="blur">
-      <div class="row full-height">
-        <div class="col-md-6 text-left padded-content">
-          <h3>Affitta la tua casa</h3>
-          <p>Se possiedi una casa per studenti <strong>Renteet</strong> ti aiuterà a tenere sotto controllo i tuoi immobili.<br>Migliora l'occuppancy e aumenta i tuoi profitti.</p>
-          <a href="/register" title="Registrati" class="btn btn-big btn-primary">Scopri di più</a>
+      <div class="content">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-5 text-center">
+              <h3 class="claim">Ottieni il massimo dal tuo <strong>immobile</strong></h3>
+              <h4 class="sub-claim">Lascia che sia renteet a organizzare la condivisione del tuo immobile.<br>Ottimizza l’occupaziome offrendo ai tuoi ospiti un’esperieza unica</h4>
+              <a href="{{url('/scopri-i-vantaggi/')}}" title="Scopri i vantaggi" class="btn btn-warning btn-how-it-works margin-top-20">Scopri i vantaggi</a>
+            </div>
+            <div class="col-md-7 text-center">
+              <img class="img-fluid" src="{{url('images/homepage/get-the-best-from-your-house.png')}}" alt="Ottieni il massimo dal tuo immobile">
+            </div>
+          </div>
         </div>
       </div>
-    </div>
   </div>
   <div class="section" id="section4">
-    <div class="blur">
-      <div class="row full-height">
-        <div class="col-md-6 offset-md-6 text-left padded-content">
-          <h3>Trova i tuoi coinquilini ideali</h3>
-          <p>Se sei uno studente fuori sede e cerchi una casa da condividere iscriviti subito ed entra nella community di <strong>Renteet</strong>.</p>
-          <a href="/register" title="Registrati" class="btn btn-big btn-primary">Scopri di più</a>
+      <div class="content">
+        <div class="container">
+          <div class="row justify-content-end">
+            <div class="col-md-6 text-center">
+              <h3 class="claim">Migliora il rapporto tra <strong>coinquilini</strong> e <strong>locatori</strong></h3>
+              <h4 class="sub-claim">Il nostro obiettivo è creare un'esperienza coinvolgente e innovativa</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  <div class="section fp-auto-height">
+    <div id="call-to-action">
+      <div class="blur">
+        <div class="content text-center">
+          <h3 class="claim">Unisciti alla community</h3>
+          <a href="/register" class="btn btn-warning btn-register margin-top-20 btn-lg" class="Iscriviti">Inizia adesso</a>
         </div>
       </div>
     </div>
+    <hr class="container" />
+    <footer>
+        <div class="container text-center">
+            <h5 class="renteet-footer">Renteet</h5>
+            <h6 class="renteet-slogan">Be friendly</h6>
+        </div>
+    </footer>
   </div>
 </div>
+
+<footer>
 @endsection
 
 @section('scripts')
 <script type="text/javascript">
   $(document).ready(function() {
     $('#fullpage').fullpage({
-      verticalCentered: false
+      verticalCentered: false,
+      slidesNavigation: true,
+      onLeave: function(index, nextIndex, direction){
+        if(nextIndex == 2) {
+          $("#home-navbar").removeClass("transparent-navbar");
+        }
+
+        if(nextIndex == 1) {
+          $("#home-navbar").addClass("transparent-navbar");
+        }
+			},
     });
   });
 </script>
