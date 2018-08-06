395bd5190d22554eda1298b127f9ae111c143505
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-08-07 00:17:18 +0200

    Modificato layout pagina notifiche

--- resources/views/profile/notifications.blade.php
+++ resources/views/profile/notifications.blade.php
@@ -7,28 +7,32 @@
 @endsection
 
 @section('content')
-<div class="container margin-top-120">
-    <div class="page-title-container">
-        <h2 class="page-title">Notifiche</h1>   
+<div class="container">
+    <div class="row">
+        <div class="page-target-container margin-top-40">
+            <h3 class="page-target">Le tue notifiche</h3>
+        </div>
     </div>
 
-    @foreach($notifications as $notification)
-        <a href="{{$notification->url}}" title="{{'Visualizza la notifica'}}" class="notification-link">
-            <div class="card shadow margin-top-40">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-1">
-                            <img class="profile-pic img-fluid" src="{{$notification->image}}">
+    <div class="margin-top-120">
+        @foreach($notifications as $notification)
+            <a href="{{$notification->url}}" title="{{'Visualizza la notifica'}}" class="notification-link">
+                <div class="card shadow margin-top-40 col-sm-8">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-auto">
+                                <img class="img-fluid rounded-circle" src="{{$notification->image}}" width="80" height="80">
+                            </div>
+                            <div class="col">
+                                <h5 class="margin-top-10">{{$notification->user->first_name}}{{$notification->user->last_name}}</h5> 
+                                <small class="notification-date">{{$notification->date}}</small>
+                            </div>
                         </div>
-                        <div class="col-sm-11">
-                            <h5 class="margin-top-10">{{$notification->user->first_name}}{{$notification->user->last_name}}</h5> 
-                            <small class="notification-date">{{$notification->date}}</small>
-                        </div>
+                        <p class="margin-top-20">{{$notification->text}}</p>
                     </div>
-                    <p class="margin-top-20">{{$notification->text}}</p>
                 </div>
-            </div>
-        </a>
-    @endforeach
+            </a>
+        @endforeach
+    </div>
 </div>
 @endsection
