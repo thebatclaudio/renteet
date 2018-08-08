0096451f3f5beade7af012c25f51429508be5f36
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-08-08 14:34:06 +0200

    Sistemato fallback

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -85,7 +85,9 @@
             </div>
         </div>
         @empty
-            <h4 class="text-muted text-center">Nessuna casa trovata</h4>
+            <div class="col">
+                <h4 class="text-muted text-center">Nessun immobile trovato</h4>
+            </div>
         @endforelse
     </div>
 </div>
