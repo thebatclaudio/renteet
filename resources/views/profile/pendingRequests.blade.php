621f6fac9051c20282a06418b4ea732adaa64e0b
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-08-21 16:59:53 +0200

    Modificato layout della pagina richieste in sospeso, seguendo lo stile della pagina notifiche (close #41)

--- resources/views/profile/pendingRequests.blade.php
+++ resources/views/profile/pendingRequests.blade.php
@@ -7,28 +7,33 @@
 @endsection
 
 @section('content')
-<div class="container margin-top-120">
-    <div class="page-title-container">
-        <h2 class="page-title">Richieste di adesione inviate in sospeso</h1>   
+<div class="container">
+    <div class="row">
+        <div class="page-target-container margin-top-40">
+            <h3 class="page-target">Richieste di adesione in sospeso</h3>
+        </div>
     </div>
 
-    @foreach($requests as $request)
-        <a href="{{$request->house->url}}" title="{{'Visualizza la casa'}}" class="notification-link">
-            <div class="card shadow margin-top-40">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-1">
-                            <img class="profile-pic img-fluid" src="{{$request->house->preview_image_url}}">
-                        </div>
-                        <div class="col-sm-11">
-                            <h5 class="margin-top-10">{{$request->house->name}}</h5> 
-                            <small class="notification-date">Inviata <strong class="date">{{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $request->pivot->created_at)->timestamp}}000</strong></small>
+    <div class="margin-top-120">
+        @foreach($requests as $request)
+            <a href="{{$request->house->url}}" title="{{'Visualizza la casa'}}" class="notification-link">
+                <div class="card shadow margin-top-40 col-sm-8">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-auto">
+                                <img class="profile-pic img-fluid" src="{{$request->house->preview_image_url}}" width="80" height="80">
+                            </div>
+                            <div class="col">
+                                <h5 class="margin-top-10">{{$request->house->name}}</h5>
+                                <small class="notification-date">Inizio soggiorno: <strong>{{\Carbon\Carbon::createFromFormat('Y-m-d', $request->pivot->start)->format('d/m/Y')}}</strong></small>
+                                <small class="notification-date">Inviata <strong class="date">{{\Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $request->pivot->created_at)->timestamp}}000</strong></small>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </a>
-    @endforeach
+            </a>
+        @endforeach
+    </div>
 </div>
 @endsection
 
