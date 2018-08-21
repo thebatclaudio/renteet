548e4f9566283124288bcfa07b6343b26bb4268e
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-08-21 18:25:49 +0200

    Create routes per i nostri valori e chi siamo. Creata pagina 'i nostri valori' (issue #70)

---
+++ resources/views/static/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Chi siamo')
+
+@section('content')
+<div class="container">
+
+<h1 class="page-title text-center margin-top-40">Chi siamo</h1>
+
+</div>
+@endsection
---
+++ resources/views/static/ourValues.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('title', 'I nostri valori')
+
+@section('styles')
+<style>
+p strong {
+    font-weight: 800;
+}
+</style>
+@endsection
+
+@section('content')
+<div class="container">
+
+<h1 class="page-title text-center margin-top-40">I nostri valori</h1>
+
+<h3>Renteet ha le idee chiare.</h3>
+
+<p class="margin-top-20">La nostra piattaforma fin dalle prime fasi della sua progettazione è stata sempre soggetta a variazioni, a volte anche significative; ma su una cosa non abbiamo mai cambiato idea: 
+<strong>su Renteet non dovrà mai esistere alcuna forma discriminatoria e di inospitalità legata a pregiudizi di ogni genere</strong>.</p>
+
+<p class="margin-top-20">Da sempre crediamo nella diversità, nella collaborazione tra le persone e in qualsiasi altro rapporto fondamentale per lo sviluppo individuale e sociale.</p>
+
+<p class="margin-top-20">Espandere questo concetto è uno degli obiettivi che ci motiva e ci spinge a dare il meglio di noi ogni giorno.</p>
+
+<p>Da qui nasce il nostro motto: <strong>be friendly</strong>.</p>
+
+</div>
+@endsection
