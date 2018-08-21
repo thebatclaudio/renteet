ab6da144f418edda2868c58658442e9d00707ccc
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-08-21 18:34:55 +0200

    Inserita pagina 'Chi siamo' (close #70)

--- resources/views/static/about.blade.php
+++ resources/views/static/about.blade.php
@@ -2,10 +2,26 @@
 
 @section('title', 'Chi siamo')
 
+@section('styles')
+<style>
+p strong {
+    font-weight: 800;
+}
+</style>
+@endsection
+
 @section('content')
 <div class="container">
 
 <h1 class="page-title text-center margin-top-40">Chi siamo</h1>
+
+<p class="margin-top-40">L’idea di <strong>renteet</strong> nasce dall’esigenza di <strong>migliorare le esperienze degli universitari alla ricerca di un appartamento in cui soggiornare</strong>. Il progetto è nato al solo scopo di fornire agli studenti delle recensioni degli immobili in cui altri studenti prima di loro hanno vissuto, garantendo una scelta più consapevole e premiando i locatori più gentili e disponibili.</p>
+
+<p class="margin-top-40">Ben presto ci siamo resi conto che il mercato di riferimento era più complesso, inserendo solo le recensioni non avremmo cambiato il modo in cui le persone vivono le esperienza di condivisione. </p>
+<p>Sarebbe riduttivo qualificare una convivenza solo come una necessità, accade qualcosa di sconvolgente quando le persone decidono di combinarsi in qualcosa di più grande. <strong>Immaginare un immobile come un piccolo spazio in cui le persone vivono e interagiscono fra di loro è stato il passo successivo.</strong></p>
+
+<p class="margin-top-40">In un’era fortemente digitalizzata convivere con persone nuove è forse il miglior modo per andare oltre l’apparenza. Si è quasi costretti a conoscere altre persone, persone che altrimenti non avremmo mai conosciuto e con le quali può nascere qualcosa di nuovo e sconvolgente. Renteet è nato così, ed è in questo modo che cerca di stimolare la magia dei rapporti umani. Potrai scegliere di convivere con chiunque vorrai fino a quando vorrai. </p>
+<p>Ogni persona è composta da un numero indefinito di elementi, questi ci rendono l’uno diverso dall’altro, in teoria quindi, le possibili combinazioni di persone sono infinite e ognuna di queste potrebbe dar vita a qualcosa di straordinario.</p>
 
 </div>
 @endsection
