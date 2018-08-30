bf58d0f4d30251eb2f00776f3cf612cdb510ff75
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-08-30 14:10:45 +0200

    Riportati link

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,10 +2,9 @@
 <ul class="navbar-nav mr-auto d-none d-sm-flex flex-row-reverse">
     @guest
         <!-- OSPITI -->
-        {{-- <li class="nav-item"><a class="nav-link" href="{{url('come-funziona')}}" title="Come funziona?">Come funziona?</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{url('come-funziona')}}" title="Come funziona?">Come funziona?</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Registrati</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Accedi</a></li> --}}
-        <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">&larr; Torna alla Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Accedi</a></li>
     @else
         <!-- LOGGATI -->
         
