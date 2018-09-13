2cc8e46a50656169a1d59712459246b9e1d18d1d
Claudio La Barbera <labarbera.claudio@gmail.com>
2018-09-13 02:21:42 +0200

    Aggiunta assistenza ai clienti con FB Messenger

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,24 @@
         @yield('styles')
     </head>
     <body>
+        <!-- Load Facebook SDK for JavaScript -->
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/it_IT/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
+        <!-- Your customer chat code -->
+        <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="1545497615573228"
+        theme_color="#099154"
+        logged_in_greeting="Ciao, come possiamo aiutarti?"
+        logged_out_greeting="Ciao, come possiamo aiutarti?">
+        </div>
 
         <header>
             <nav class="main-navbar navbar navbar-expand-md navbar-dark fixed-top bg-dark">
